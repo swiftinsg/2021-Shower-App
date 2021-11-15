c9e3e820c5f9844108597e42c30d8755f566b280
James Chan <james_chan@rhvs.edu.sg>
2021-11-15 10:15:16 +0800

    added graph screen

---
+++ Shower App/Graph.swift
@@ -0,0 +1,20 @@
+//
+//  Graph.swift
+//  Shower App
+//
+//  Created by Student on 15/11/21.
+//
+
+import SwiftUI
+
+struct Graph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Graph_Previews: PreviewProvider {
+    static var previews: some View {
+        Graph()
+    }
+}
